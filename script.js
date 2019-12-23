faa0feaa518607d888e58ebb2d157279b922df32
abtaha-bookmark-app <59124016+abtaha-bookmark-app@users.noreply.github.com>
2019-12-23 11:29:30 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,22 +8,37 @@ let nameInput = document.getElementById("nameInput");
 if (localStorage.getItem("bookmarkApp") === "") {
 	bookmarks = "";
 } else {
-	document.getElementById("lists").innerHTML = localStorage.getItem("bookmarkApp");
+	document.getElementById("lists").innerHTML = localStorage.getItem(
+		"bookmarkApp"
+	);
 }
 
+const isValidUrl = str => {
+	regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+	if (regexp.test(str)) {
+		return str;
+	} else {
+		alert("Invalid URL");
+		modal.style.display = "block";
+		return false;
+	}
+};
+
 function addToDocument() {
-	if (
-		linkInput.value.replace(/\s+/g, "") != "" &&
-		nameInput.value.replace(/\s+/g, "") != ""
-	) {
+	modal.style.display = "none";
+
+	let url = isValidUrl("https://" + linkInput.value);
+	let name = nameInput.value === "" ? url : nameInput.value;
+
+	if (url) {
 		document.getElementById(
 			"lists"
-		).innerHTML += `<a href=http://${linkInput.value}>${nameInput.value}</a>`;
+		).innerHTML += `<a href=${url} target="_blank">${name}</a>`;
 
-		localStorage.setItem("bookmarkApp", document.getElementById("lists").innerHTML);
-	} else {
-		alert("Please fill the Inputs");
-		modal.style.display = "block";
+		localStorage.setItem(
+			"bookmarkApp",
+			document.getElementById("lists").innerHTML
+		);
 	}
 }
 
@@ -33,12 +48,10 @@ function addBookmark() {
 
 span.onclick = function() {
 	modal.style.display = "none";
-	addToDocument();
 };
 
 window.onclick = function(event) {
 	if (event.target == modal) {
-		modal.style.display = "none";
 		addToDocument();
 	}
 };
